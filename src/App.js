0be0562f53f37f0a82b637518c32ef44704299b7
krunalsukhwani <krunal.sukhwani@gmail.com>
2024-09-11 21:36:09 -0400

    Added Static Task List

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React from "react";
+import "./App.css";
 
 const App = () => {
-  return <h1>Hello Friends, Welcome to COMP 229 - Advanced Web Development</h1>;
+  return (
+    <div className="course-tasks">
+      <h2>Task List</h2>
+      <ul className="task-list">
+        <li>Attend COMP 229 Class</li>
+        <li>Complete Assignment</li>
+        <li>Play Video Game</li>
+      </ul>
+    </div>
+  );
 };
 
 export default App;
